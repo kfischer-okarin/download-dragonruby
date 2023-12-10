07e166f451e2934e7898ad74644610c0777baa2f
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-12-10 10:08:24 +0900

    Automatically determine platform

--- download_dragonruby.sh
+++ download_dragonruby.sh
@@ -4,8 +4,19 @@ set -e
 
 version=$1
 
+if [[ $RUNNER_OS == "Linux" ]]; then
+  platform="linux-amd64"
+elif [[ $RUNNER_OS == "macOS" ]]; then
+  platform="macos"
+elif [[ $RUNNER_OS == "Windows" ]]; then
+  platform="windows-amd64"
+else
+  echo "Unknown platform $RUNNER_OS"
+  exit 1
+fi
+
 base_url="https://github.com/kfischer-okarin/dragonruby-for-ci/releases/download/$version"
-filename="dragonruby-for-ci-$version-standard-linux-amd64.zip"
+filename="dragonruby-for-ci-$version-standard-$platform.zip"
 
 curl -L -o dragonruby.zip $base_url/$filename
 unzip dragonruby.zip
